b35284701d447fe323d0763afb76bb536c7c2a41
sjf1017 <susanfeitler@susans-Air.fios-router.home>
2016-10-26 09:19:04 -0400

    Home work Submission

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,23 @@
+var h1 = '<h1>'+ 10*10  + '</h1>';
+var h2 ='<h2>' + 100/10 + '</h2>';
+//var h3 ='<h3>' + 50-17  + '</h3>';
+//var myName = '<p>' susan feitler '</p>';
+
+
+
+document.write(h1);
+document.write(h2);
+//document.write(h3);
+//document.write (myName);
+ 
+function hello () {
+	  var greeting = 'hello!';
+
+	  greeting = 'hello, world!';
+
+	  alert(greeting);
+	}
+hello();
+document.getElementById('title').style.color = 'red';
+document.querySelector('#text-chunk').style.color = 'green';
+
