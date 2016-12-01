8566b9563a29f58e977b4317213be1d8d5118d04
sjf1017 <susanfeitler@susans-Air.fios-router.home>
2016-12-01 16:39:14 -0500

    Dinner Party Success - In progress

--- class-work/class18/google-maps-example/js/app.js
+++ class-work/class18/google-maps-example/js/app.js
@@ -1,5 +1,38 @@
-$(document).ready(function() {
+var myMap;
+  function initMap() {
 
-})
+//	google.maps.Map(element,options)
 
+        myMap = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.730610, lng: -73.935242},
+        zoom: 12
+     });
+
+    var marker = new google.maps.Marker({
+	  position: {lat:40.730610, lng: -73.935242},
+	  map: myMap,
+	  title: 'New York, NY'
+	});
+	var theMet = new google.maps.Marker({
+		map:myMap,
+		position:{
+			lat:40.779416,
+			lng:-73.963201
+
+		},
+		title:'the Met'
+	})
+
+	marker.addListener('click',function(){
+		$('#map')
+		.css('height','50%')
+		.css('width','50%')
+		
+		myMap.setZoom(8);
+		myMap.setCenter(marker.getPosition());
+
+	});
+   }
+
+initMap();
 
