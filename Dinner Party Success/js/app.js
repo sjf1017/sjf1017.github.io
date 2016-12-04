a9e98359def1f91e121fbf52ff7912bdb8d6d3dc
sjf1017 <susanfeitler@susans-Air.fios-router.home>
2016-12-04 09:04:34 -0500

    Final Project Updated

--- Dinner Party Success/js/app.js
+++ Dinner Party Success/js/app.js
@@ -15,6 +15,28 @@ $(document).ready(function () {
 		
 	})
 
+	var carouselContainer = $('.carousel');
+	var slideInterval = 3000;
+
+	function toggleCaption() {
+  	 $('.carousel-caption').hide();
+  	 var caption = carouselContainer.find('.active').find('.carousel-caption');
+   	 caption.delay(500).toggle("slide", {direction:'right'});
+	}
+
+
+	carouselContainer.carousel({
+  	 interval: slideInterval,
+  	 cycle: true,
+  	 //Pauses the carousel from going through the next slide when the mouse pointer enters the carousel
+  	 pause: "hover"
+	})
+	//occurs when carousel is going to slide from one item to another
+	.on('slid.bs.carousel', function() {
+     toggleCaption();
+});
+
+
 	
 	// $(".carousel-indicators").click(function(){
 	// 	$(".carousel").carousel('prev');
