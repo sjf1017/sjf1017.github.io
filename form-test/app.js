9bbe0ba379c5326f4442dab463512d0b09f63f99
sjf1017 <susanfeitler@susans-MacBook-Air.local>
2016-12-05 19:13:50 -0500

    Final presentations

--- form-test/app.js
+++ form-test/app.js
@@ -1,26 +1,64 @@
 $(document).ready(function () {
 
-	$('#myForm').submit(function(event){
-		event.preventDefault();
+	(function( $ ) {
 
-		console.log(event);
+	//Function to animate slider captions 
+	function doAnimations( elems ) {
+		//Cache the animationend event in a variable
+		var animEndEv = 'webkitAnimationEnd animationend';
+		
+		elems.each(function () {
+			var $this = $(this),
+				$animationType = $this.data('animation');
+			$this.addClass($animationType).one(animEndEv, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+	
+	//Variables on page load 
+	var $myCarousel = $('#carousel-example-generic'),
+		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+		
+	//Initialize carousel 
+	$myCarousel.carousel();
+	
+	//Animate captions in first slide on page load 
+	doAnimations($firstAnimatingElems);
+	
+	//Pause carousel  
+	$myCarousel.carousel('pause');
+	
+	
+	//Other slides to be animated on carousel slide event 
+	$myCarousel.on('slide.bs.carousel', function (e) {
+		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+		doAnimations($animatingElems);
+	});  
+	
+})(jQuery);
 
-		var name = $('#name-input').val();
+	// $('#myForm').submit(function(event){
+	// 	event.preventDefault();
 
-		$('#target').text(name);
+	// 	console.log(event);
+
+	// 	var name = $('#name-input').val();
+
+	// 	$('#target').text(name);
 
 
-	})
+	// })
 
-	$('#name-input').keydown(function(event){
-		console.log(event);
-		event.preventDefault;
-	})
+	// $('#name-input').keydown(function(event){
+	// 	console.log(event);
+	// 	event.preventDefault;
+	// })
 
-	$('#social a').click(function(event){
-		console.log(event)
-		event.preventDefault();
+	// $('#social a').click(function(event){
+	// 	console.log(event)
+	// 	event.preventDefault();
 
-	})
+	// })
 
 })
